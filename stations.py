0b94d272afc30ee66cf32e6004e128a998ae191f
george <georg.papageorg@gmail.com>
2024-07-11 00:44:19 +0300

    Added Station

--- stations.py
+++ stations.py
@@ -1,3 +1,3 @@
-stations = {"RadioParadise": "https://stream.radioparadise.com/flacm",
-            "RadioZeppelin":"https://radiostreaming.ert.gr/ert-zeppelin",
-            "ImagineRadio": ""}
+stations = {"RadioZeppelin":"https://radiostreaming.ert.gr/ert-zeppelin",
+            "RadioParadise": "https://stream.radioparadise.com/flacm",
+            "ImagineRadio": "https://imagine897.radioca.st/stream"}
